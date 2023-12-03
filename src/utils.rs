78c0c3c941a8d6d2a95f7364791bfe078dc150a0
Petr Volnov <neafiol.io@gmail.com>
2023-12-03 13:17:20 -0800

    0.36

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use near_indexer_primitives::types;
+use near_o11y::WithSpanContextExt;
 
 /// Fetches the status to retrieve `latest_block_height` to determine if we need to fetch
 /// entire block or we already fetched this block.
@@ -6,9 +7,10 @@ pub(crate) async fn fetch_latest_block(
     client: &actix::Addr<near_client::ViewClientActor>,
 ) -> anyhow::Result<u64> {
     let block = client
-        .send(near_client::GetBlock(types::BlockReference::Finality(
-            types::Finality::Final,
-        )))
+        .send(
+            near_client::GetBlock(types::BlockReference::Finality(types::Finality::Final))
+                .with_span_context(),
+        )
         .await??;
     Ok(block.header.height)
 }
